a767af38466bdbdbf999eb71f99672b98e804718
David <trashmode69@gmail.com>
2021-01-09 19:42:19 +0200

    This is my first exercise

--- Lesson 1/Targil 1.py
+++ Lesson 1/Targil 1.py
@@ -1,5 +1,13 @@
-print ("hello world")
+num = int(input("Enter a number with 4 digits"))
 
+# This is Alafim
+print("Alafim=" + str(num // 1000))
 
-for i in  range(10):
-       print(i)
+# Tihs is Meot
+print("Meot=" + str((num // 100) % 10))
+
+# This si Asarot
+print("Asarot=" + str((num % 100) // 10))
+
+# This is Ahadot
+print("Ahadot=" + str(num % 10))
